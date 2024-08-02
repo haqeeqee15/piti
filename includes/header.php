8a95123b671facc1b3ea9ed75c2f5686b515fdd6
haqiqi.muhammad <maulia.haqiqi@gmail.com>
2024-08-02 22:17:37 +0700

    batch 1

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<?php include "head.php" ?>
+<body>
+    <!-- ==== HEADER ==== -->
+    <header class="container header">
+      <!-- ==== NAVBAR ==== -->
+      <nav class="nav">
+        <div class="logo">
+          <h2>Piti.</h2>
+        </div>
+
+
+        <button class="toggle_btn" id="toggle_btn">
+          <i class="ri-menu-line"></i>
+        </button>
+      </nav>
+    </header>
